04297319bad9743d82e4a438ac9de91e99b9d5fd
amlanxbghn <amlanxconnect@gmail.com>
2024-08-14 02:42:16 +0530

    logo ticker animation

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,38 +1,93 @@
-import Image from "next/image";
+"use client";
 import acmeLogo from '../assets/images/acme.png';
 import quantumLogo from '../assets/images/quantum.png'
 import echoLogo from '../assets/images/echo.png';
 import celestialLogo from '../assets/images/celestial.png';
 import pulseLogo from '../assets/images/pulse.png';
 import apexLogo from '../assets/images/apex.png';
-
-const images = [
-    { src: acmeLogo, alt: "Acme Logo" },
-    { src: quantumLogo, alt: "Quantum Logo" },
-    { src: echoLogo, alt: "Echo Logo" },
-    { src: celestialLogo, alt: "Celestial Logo" },
-    { src: pulseLogo, alt: "Pulse Logo" },
-    { src: apexLogo, alt: "Apex Logo" },
-];
+import Image from "next/image";
+import { motion } from 'framer-motion';
 
 export const LogoTicker = () => {
     return (
-        <div className="py-8">
+        <div className="py-8 md:py-12">
             <div className="container">
                 <h2 className="text-xl text-center text-black/60">
                     Trusted by Founders and Teams at:
                 </h2>
-                <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#fff,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#fff,rgb(0,0,0,0))]">
-                    <div className="flex justify-center gap-16">
-                        {images.map(({ src, alt }) => (
-                            <Image
-                                key={alt}
-                                src={src}
-                                alt={alt}
-                                className="flex-none h-8 w-auto"
-                            />
-                        ))}
-                    </div>
+                <div className="py-8 flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+                    <motion.div className="flex gap-14 flex-none pr-14" 
+                        animate={{
+                            translateX: '-50%'
+                        }}
+                        transition={{
+                            duration: 10,
+                            repeat: Infinity,
+                            ease: "linear",
+                            repeatType: "loop",
+                        }}
+                    >
+                        <Image
+                            src={acmeLogo}
+                            alt='Acme Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={quantumLogo}
+                            alt='Quantum Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={echoLogo}
+                            alt='Echo Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={celestialLogo}
+                            alt='Celestial Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={pulseLogo}
+                            alt='Pulse Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={apexLogo}
+                            alt='Apex Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={acmeLogo}
+                            alt='Acme Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={quantumLogo}
+                            alt='Quantum Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={echoLogo}
+                            alt='Echo Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={celestialLogo}
+                            alt='Celestial Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={pulseLogo}
+                            alt='Pulse Logo'
+                            className='h-8 w-auto'
+                        />
+                        <Image
+                            src={apexLogo}
+                            alt='Apex Logo'
+                            className='h-8 w-auto'
+                        />
+                    </motion.div>
                 </div>
             </div>
         </div>
