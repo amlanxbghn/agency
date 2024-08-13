9b4ee572333cfcc056771d43ce591f386f227499
amlanxbghn <amlanxconnect@gmail.com>
2024-08-13 05:35:52 +0530

    Hero Reveal Animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,26 @@
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
 export const Hero = () => {
     return (
-        <div className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
+        <div className="py-24">
             <div className="container">
                 <div className="flex justify-center">
                     <div className="text-lg inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
-                        <span className="text-green-500 animate-blink">●&nbsp;</span> 
+                        <span className="text-green-500 animate-blink">●&nbsp;</span>
                         We are open to work
                     </div>
                 </div>
-                <h1 className=" text-center text-4xl md:text-6xl md:leading-[70px] font-bold tracking-tighter max-w-[950px] mx-auto bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5 py-2">
-                    Crafting Next-Gen Landing Pages for Early-Stage Startups
-                </h1>
-                <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5 max-w-[720px] mx-auto">
-                    Helping Founders and Early-Stage Startups build Fully Functional Responsive Websites for 1/3 of the Cost and get 3x Conversions.
+
+                <div className="text-center text-6xl md:text-8xl md:leading-[95px] font-bold tracking-tighter max-w-[950px] mx-auto mt-5 py-2">
+                    <TextGenerateEffect
+                        className=""
+                        words="Crafting Next-Gen Landing Pages"
+                        duration={1}
+                    />
+                </div>
+
+                <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5 max-w-[750px] mx-auto">
+                    Helping Founders and Early-Stage Startups build Fully Functional Responsive Websites for 1/3 of the Cost and get 3x More Conversions.
                 </p>
                 <div className="flex justify-center mt-8">
                     <button className="text-xl bg-black text-white py-3 px-5 rounded-lg font-medium shadow-lg">Book a call</button>
